4accabb9fa5feae7e31aa29b11e5d1f8db9f9370
suruchisinha <suruchisinha.10@gmail.com>
2017-08-09 21:27:22 +0530

    Resume test

--- src/Drupal/firefox.java
+++ src/Drupal/firefox.java
@@ -1,10 +1,15 @@
 package Drupal;
 
+import static org.testng.Assert.assertEquals;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.By.ByXPath;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -12,33 +17,63 @@ import org.testng.annotations.Test;
 
 
 public class firefox {
+	
 	public static WebDriver driver; 
 	
 		// TODO Auto-generated method stub
 		
-		//System.setProperty("webdriver.chrome.driver","C:\\Users\\Groupten\\Downloads\\chromedriver\\chromedriver.exe");
-	@BeforeMethod	
-	public void main() {
+		
+	@BeforeClass	
+	public void FFLaunch() {
+		
 		driver=new FirefoxDriver();
 		driver.manage().window().maximize();
-		driver.get("http://drupalhope.dd:8083/");
+		System.out.println("Firefox Launched ");
+		
 		}
+	
+		
 		@Test
 		public void Login() {
-		String URL="http://drupalhope.dd:8083/resume/myform";
-		
+			
+	    driver.get("http://drupalhope.dd:8083/");
 		driver.findElement(By.xpath(".//*[@id='block-bartik-account-menu']/div/ul/li/a")).click();
 		driver.findElement(By.xpath(".//*[@id='edit-name']")).sendKeys("hope");
 		driver.findElement(By.xpath(".//*[@id='edit-pass']")).sendKeys("hope");
 		driver.findElement(By.xpath(".//*[@id='edit-submit']")).click();
-		String login = driver.findElement(By.xpath(".//*[@id='block-bartik-account-menu']/div[2]/ul/li[2]/a")).getText();
-		Assert.assertEquals(login,"Log out");
+		boolean LogOut = driver.findElement(By.xpath(".//*[@id='block-bartik-account-menu']/div[2]/ul/li[2]/a")).isDisplayed();
+		Assert.assertEquals(LogOut,true,"User failed to login");
+		}
 		
-		driver.findElement(By.xpath(".//*[@id='block-bartik-main-menu']/div[2]/ul/li[2]/a")).click();
-		String str1=driver.getCurrentUrl();
-		Assert.assertEquals(URL,str1);
-	}
+		@Test
+		public void ResumeUI() {
+		driver.get("http://drupalhope.dd:8083/resume/myform");
+		driver.findElement(By.xpath(".//*[@id='edit-candidate-name']")).sendKeys("Naina");
+		driver.findElement(By.xpath(".//*[@id='edit-candidate-mail']")).sendKeys("Naina@gmail.com");
+		driver.findElement(By.xpath(".//*[@id='edit-candidate-number']")).sendKeys("9876543210");
+		driver.findElement(By.xpath(".//*[@id='edit-candidate-dob']")).sendKeys("2017-08-23");
+		driver.findElement(By.xpath(".//*[@id='edit-candidate-dob']")).sendKeys("Female");
+		driver.findElement(By.xpath(".//*[@id='edit-candidate-confirmation-yes']")).click();
+		driver.findElement(By.xpath(".//*[@id='edit-candidate-copy']")).click();
+		driver.findElement(By.xpath(".//*[@id='edit-submit']")).click();
+		
+		
+		String CurrentUrl = driver.getCurrentUrl();
+		String ExpectedUrl = "http://drupalhope.dd:8083/user/1";
+		
+		Assert.assertEquals(CurrentUrl, ExpectedUrl,"Error in submitting Form ");
+		
+		String Message=driver.findElement(By.xpath(".//*[@id='page']/div[1]/aside/div/div/div")).getText();
+		System.out.println(Message);
+			
+		 }
 		 
+		@AfterClass	
+		public void CloseBrowser() {
+			
+			driver.quit();
+			System.out.println("Firefox Closed");
+			}
 		
 		
 		
